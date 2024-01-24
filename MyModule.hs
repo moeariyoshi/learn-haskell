24c1909478a0e4f936865f16d1ea5aae4fe0a3f8
moeariyoshi <moe.ariyoshi@Michaelangelo.local>
2024-01-24 00:24:47 -0500

    add example module!

---
+++ MyModule.hs
@@ -0,0 +1,10 @@
+Add `import MyModule` into the file you need it for!
+
+-- Module definition
+module MyModule (add, subtract) where
+
+add :: Int -> Int -> Int
+add x y = x + y
+
+subtract :: Int -> Int -> Int
+subtract x y = x - y
